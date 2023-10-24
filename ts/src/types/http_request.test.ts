858d5b38b29bdb46274a94f640649a078bf1a483
Meno Abels <meno.abels@adviser.com>
2023-10-24 14:04:45 +0200

    chore: complete HttpURL implementation

--- ts/src/types/http_request.test.ts
+++ ts/src/types/http_request.test.ts
@@ -1,34 +1,33 @@
 import { HttpURL } from "./http_request";
 
 it("test from string", () => {
-  const url = HttpURL.parse("http://localhost:8080/abc/def?m=1&n=2").unwrap().AsJsURL();
-  expect(url.protocol).toBe("http:");
-  expect(url.hostname).toBe("localhost");
-  expect(url.port).toBe("8080");
-  expect(url.pathname).toBe("/abc/def");
-  expect(Object.fromEntries(url.searchParams.entries())).toEqual({ m: "1", n: "2" });
+  const url = HttpURL.parse("http://localhost:8080/abc/def?m=1&n=2").unwrap();
+  expect(url.Scheme).toBe("http:");
+  expect(url.Hostname).toBe("localhost");
+  expect(url.Port).toBe("8080");
+  expect(url.Path).toBe("/abc/def");
+  expect(Object.fromEntries(url.SearchParams.Entries())).toEqual({ m: "1", n: "2" });
 });
 
 it("test from object", () => {
   const inner = HttpURL.parse("http://localhost:8080/abc/def?m=1&n=2");
-  const url = HttpURL.parse(inner).unwrap().AsJsURL();
+  const url = HttpURL.parse(inner).unwrap();
   inner.Ok().SetPath("/dkdkdkd");
-  expect(url.protocol).toBe("http:");
-  expect(url.hostname).toBe("localhost");
-  expect(url.port).toBe("8080");
-  expect(url.pathname).toBe("/abc/def");
-  expect(Object.fromEntries(url.searchParams.entries())).toEqual({ m: "1", n: "2" });
+  expect(url.Scheme).toBe("http:");
+  expect(url.Hostname).toBe("localhost");
+  expect(url.Port).toBe("8080");
+  expect(url.Path).toBe("/abc/def");
+  expect(Object.fromEntries(url.SearchParams.Entries())).toEqual({ m: "1", n: "2" });
 });
 
 it("test setPathname", () => {
   const hurl = HttpURL.parse("http://localhost:8080/abc/def?m=1&n=2").unwrap();
   hurl.SetPath("bla/", "/bli", "bl//ub");
-  const url = hurl.AsJsURL();
-  expect(url.protocol).toBe("http:");
-  expect(url.hostname).toBe("localhost");
-  expect(url.port).toBe("8080");
-  expect(url.pathname).toBe("/bla/bli/bl//ub");
+  expect(hurl.Scheme).toBe("http:");
+  expect(hurl.Hostname).toBe("localhost");
+  expect(hurl.Port).toBe("8080");
+  expect(hurl.Path).toBe("/bla/bli/bl//ub");
   hurl.SetPath("//////bla/", "/bli", "bl//ub");
   expect(hurl.Path).toBe("//////bla/bli/bl//ub");
-  expect(Object.fromEntries(url.searchParams.entries())).toEqual({ m: "1", n: "2" });
+  expect(Object.fromEntries(hurl.SearchParams.Entries())).toEqual({ m: "1", n: "2" });
 });
--- ts/src/types/http_request.ts
+++ ts/src/types/http_request.ts
@@ -1,6 +1,164 @@
 import { HttpHeader } from "./http_header";
 import { result } from "wueste";
 
+export class HttpURLSearchParams {
+  readonly _urlSearchParams: URLSearchParams;
+  constructor(refInit: URLSearchParams) {
+    this._urlSearchParams = refInit;
+  }
+  /**
+   * Append a new name-value pair to the query string.
+   */
+  Append(name: string, value: string): void {
+    this._urlSearchParams.append(name, value);
+  }
+
+  /**
+   * If `value` is provided, removes all name-value pairs
+   * where name is `name` and value is `value`..
+   *
+   * If `value` is not provided, removes all name-value pairs whose name is `name`.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  Delete(name: string, value?: string): void {
+    this._urlSearchParams.delete(name);
+  }
+  /**
+   * Returns an ES6 `Iterator` over each of the name-value pairs in the query.
+   * Each item of the iterator is a JavaScript `Array`. The first item of the `Array`is the `name`, the second item of the `Array` is the `value`.
+   *
+   * Alias for `urlSearchParams[@@iterator]()`.
+   */
+  Entries(): IterableIterator<[string, string]> {
+    return this._urlSearchParams.entries();
+  }
+  /**
+   * Iterates over each name-value pair in the query and invokes the given function.
+   *
+   * ```js
+   * const myURL = new URL('https://example.org/?a=b&#x26;c=d');
+   * myURL.searchParams.forEach((value, name, searchParams) => {
+   *   console.log(name, value, myURL.searchParams === searchParams);
+   * });
+   * // Prints:
+   * //   a b true
+   * //   c d true
+   * ```
+   * @param fn Invoked for each name-value pair in the query
+   * @param thisArg To be used as `this` value for when `fn` is called
+   */
+  ForEach<TThis = this>(
+    callback: (this: TThis, value: string, name: string, searchParams: URLSearchParams) => void,
+    thisArg?: TThis,
+  ): void {
+    return this._urlSearchParams.forEach(callback, thisArg);
+  }
+  /**
+   * Returns the value of the first name-value pair whose name is `name`. If there
+   * are no such pairs, `null` is returned.
+   * @return or `null` if there is no name-value pair with the given `name`.
+   */
+  Get(name: string): string | null {
+    return this._urlSearchParams.get(name);
+  }
+  /**
+   * Returns the values of all name-value pairs whose name is `name`. If there are
+   * no such pairs, an empty array is returned.
+   */
+  GetAll(name: string): string[] {
+    return this._urlSearchParams.getAll(name);
+  }
+  /**
+   * Checks if the `URLSearchParams` object contains key-value pair(s) based on`name` and an optional `value` argument.
+   *
+   * If `value` is provided, returns `true` when name-value pair with
+   * same `name` and `value` exists.
+   *
+   * If `value` is not provided, returns `true` if there is at least one name-value
+   * pair whose name is `name`.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  Has(name: string, value?: string): boolean {
+    return this._urlSearchParams.has(name);
+  }
+  /**
+   * Returns an ES6 `Iterator` over the names of each name-value pair.
+   *
+   * ```js
+   * const params = new URLSearchParams('foo=bar&#x26;foo=baz');
+   * for (const name of params.keys()) {
+   *   console.log(name);
+   * }
+   * // Prints:
+   * //   foo
+   * //   foo
+   * ```
+   */
+  Keys(): IterableIterator<string> {
+    return this._urlSearchParams.keys();
+  }
+  /**
+   * Sets the value in the `URLSearchParams` object associated with `name` to`value`. If there are any pre-existing name-value pairs whose names are `name`,
+   * Set the first such pair's value to `value` and remove all others. If not,
+   * append the name-value pair to the query string.
+   *
+   * ```js
+   * const params = new URLSearchParams();
+   * params.append('foo', 'bar');
+   * params.append('foo', 'baz');
+   * params.append('abc', 'def');
+   * console.log(params.toString());
+   * // Prints foo=bar&#x26;foo=baz&#x26;abc=def
+   *
+   * params.Set('foo', 'def');
+   * params.Set('xyz', 'opq');
+   * console.log(params.toString());
+   * // Prints foo=def&#x26;abc=def&#x26;xyz=opq
+   * ```
+   */
+  Set(name: string, value: string): void {
+    this._urlSearchParams.set(name, value);
+  }
+  /**
+   * The total number of parameter entries.
+   * @since v19.8.0
+   */
+  get size(): number {
+    return this._urlSearchParams.size;
+  }
+  /**
+   * Sort all existing name-value pairs in-place by their names. Sorting is done
+   * with a [stable sorting algorithm](https://en.wikipedia.org/wiki/Sorting_algorithm#Stability), so relative order between name-value pairs
+   * with the same name is preserved.
+   *
+   * This method can be used, in particular, to increase cache hits.
+   *
+   * ```js
+   * const params = new URLSearchParams('query[]=abc&#x26;type=search&#x26;query[]=123');
+   * params.sort();
+   * console.log(params.toString());
+   * // Prints query%5B%5D=abc&#x26;query%5B%5D=123&#x26;type=search
+   * ```
+   * @since v7.7.0, v6.13.0
+   */
+  Sort(): void {
+    this._urlSearchParams.sort();
+  }
+  /**
+   * Returns the search parameters serialized as a string, with characters
+   * percent-encoded where necessary.
+   */
+  toString(): string {
+    return this._urlSearchParams.toString();
+  }
+  /**
+   * Returns an ES6 `Iterator` over the values of each name-value pair.
+   */
+  Values(): IterableIterator<string> {
+    return this._urlSearchParams.values();
+  }
+}
+
 export class HttpURL {
   readonly _url: URL;
 
@@ -31,25 +189,45 @@ export class HttpURL {
   get Scheme(): string {
     return this._url.protocol;
   }
+  SetScheme(scheme: string) {
+    this._url.protocol = scheme;
+  }
+  get Href(): string {
+    return this._url.href;
+  }
+  SetHref(href: string) {
+    this._url.href = href;
+  }
+  get Origin(): string {
+    return this._url.origin;
+  }
+  get Username(): string {
+    return this._url.username;
+  }
+  SetUsername(username: string) {
+    this._url.username = username;
+  }
+  get Password(): string {
+    return this._url.password;
+  }
+  SetPassword(password: string) {
+    this._url.password = password;
+  }
   get Host(): string {
     return this._url.host;
+  }
+  SetHost(host: string) {
+    this._url.host = host;
   }
   get Hostname(): string {
     return this._url.hostname;
   }
+  SetHostname(hostname: string) {
+    this._url.hostname = hostname;
+  }
   get Path(): string {
     return this._url.pathname;
   }
-  get Port(): string {
-    return this._url.port;
-  }
-  get SearchParams(): URLSearchParams {
-    return this._url.searchParams;
-  }
-  get Search(): string {
-    return this._url.search;
-  }
-
   SetPath(...parts: string[]) {
     for (let i = 1; i < parts.length; i++) {
       if (parts[i - 1].endsWith("/")) {
@@ -61,6 +239,27 @@ export class HttpURL {
     }
     this._url.pathname = parts.join("/");
     return this._url.pathname;
+  }
+  get Port(): string {
+    return this._url.port;
+  }
+  SetPort(port: string) {
+    this._url.port = port;
+  }
+  get SearchParams(): HttpURLSearchParams {
+    return new HttpURLSearchParams(this._url.searchParams);
+  }
+  get Search(): string {
+    return this._url.search;
+  }
+  SetSearch(search: string) {
+    this._url.search = search;
+  }
+  get Hash(): string {
+    return this._url.hash;
+  }
+  SetHash(hash: string) {
+    this._url.hash = hash;
   }
 
   AsJsURL(): URL {
